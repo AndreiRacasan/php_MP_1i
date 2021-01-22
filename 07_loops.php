aa6d93f62aa96c21fae41d4b2672d7333c48a711
Andrei Racasan <aracasan@outlook.com>
2021-01-22 22:41:48 +0000

    complete loops

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,32 @@
 <?php
 
 // while
+while (false) {
+  echo "don't replace false with true";
+}
 
 // Loop with $counter
+$c = 1;
+
+while ($c <= 10) {
+  echo $c;
+  $c++;
+}
 
 // do - while
+do {
+  echo $c;
+  $c++;
+} while ($c <= 20);
 
 // for
+for ($i = 1; $i <= 10; $i++) {
+  echo $i;
+}
 
 // foreach
+$x = ['a', 'b', 'c'];
 
-// Iterate Over associative array.
+foreach ($x as $m => $print) {
+  echo $print;
+}
