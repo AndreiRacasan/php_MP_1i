172ef10de5446262762aac93c4456a291a584285
Andrei Racasan <aracasan@outlook.com>
2021-01-23 12:23:51 +0000

    complete file system

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,17 +1,28 @@
 <?php
 // Magic constants
+echo __DIR__;
+echo __FILE__;
+
+echo __LINE__;
 
 // Create directory
+// mkdir('test');
 
 // Rename directory
+// rename('test', 'test2');
 
 // Delete directory
+// rmdir('test2');
 
 // Read files and folders inside directory
+echo file_get_contents('lorem.txt');
 
 // file_get_contents, file_put_contents
+scandir('./');
+file_put_contents('test.txt', 'content');
 
 // file_get_contents from URL
+// file_get_contents('url');
 
 // https://www.php.net/manual/en/book.filesystem.php
 // file_exists
