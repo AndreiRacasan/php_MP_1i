afd63d25016b5874d734dd16de338040539a0950
Andrei Racasan <aracasan@outlook.com>
2021-01-23 11:48:34 +0000

    complete dates;

--- 09_dates.php
+++ 09_dates.php
@@ -1,13 +1,16 @@
 <?php
 
 // Print current date
+echo date('Y-m-d H:i:s');
 
 // Print yesterday
+echo date('Y-m-d H:i:s', time() - 86400);
 
 // Different format: https://www.php.net/manual/en/function.date.php
+echo date('F j Y, H:i:s');
 
 // Print current timestamp
+echo time();
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
-
-// Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
+date_parse('2021-01-23 11:45');
